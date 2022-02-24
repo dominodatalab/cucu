eaff6007e1419803e0c8df087a8499246f7b8beb
rlgomes-ddl <rodney.gomes@dominodatalab.com>
2022-02-23 23:16:04 -0800

    silence the HTTP server logs

--- src/cucu/steps/webserver_steps.py
+++ src/cucu/steps/webserver_steps.py
@@ -5,9 +5,15 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from threading import Thread
 
 
+class QuietHTTPRequestHandler(SimpleHTTPRequestHandler):
+
+    def log_message(self, format, *args):
+        return
+
+
 @step('I start a webserver on port "{port}" at directory "{directory}"')
 def run_webserver_for_scenario(context, port, directory):
-    handler = partial(SimpleHTTPRequestHandler, directory=directory)
+    handler = partial(QuietHTTPRequestHandler, directory=directory)
     httpd = HTTPServer(('', int(port)), handler)
     thread = Thread(target=httpd.serve_forever)
     thread.start()
