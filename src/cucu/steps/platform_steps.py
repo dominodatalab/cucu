0434b8e3d782376143eeda49b66eb4f56da4eb69
rlgomes-ddl <rodney.gomes@dominodatalab.com>
2022-04-13 22:03:49 -0700

    step should refer to scenario and not test

--- src/cucu/steps/platform_steps.py
+++ src/cucu/steps/platform_steps.py
@@ -3,13 +3,13 @@ import sys
 from cucu import step
 
 
-@step("I skip this test if not on mac")
-def skip_test_if_on_mac(ctx):
+@step("I skip this scenario if not on mac")
+def skip_scenario_if_on_mac(ctx):
     if "darwin" not in sys.platform:
         ctx.scenario.skip(reason="skipping scenario since we're not on a mac")
 
 
-@step("I skip this test if not on linux")
-def skip_test_if_on_linuxc(ctx):
+@step("I skip this scenario if not on linux")
+def skip_scenario_if_on_linuxc(ctx):
     if "linux" not in sys.platform:
         ctx.scenario.skip(reason="skipping scenario since we're not on a linux")
