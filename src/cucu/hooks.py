e144651d98f1c05b9778e0a16650245fdc943cd1
rlgomes-ddl <98488861+rlgomes-ddl@users.noreply.github.com>
2022-04-20 10:44:14 -0700

    expand and fix various before/after scenario/step hooks (#84)
    * found a few bugs and also decided to finish up expanding the hooks for
      before/after steps as it could be useful for a few things

--- src/cucu/hooks.py
+++ src/cucu/hooks.py
@@ -6,21 +6,25 @@ import sys
 from cucu.config import CONFIG
 
 
-def init_hook_variables():
+def init_global_hook_variables():
     CONFIG["__CUCU_BEFORE_SCENARIO_HOOKS"] = []
     CONFIG["__CUCU_AFTER_SCENARIO_HOOKS"] = []
-    CONFIG["__CUCU_BEFORE_THIS_SCENARIO_HOOKS"] = []
-    CONFIG["__CUCU_AFTER_THIS_SCENARIO_HOOKS"] = []
 
     CONFIG["__CUCU_BEFORE_STEP_HOOKS"] = []
     CONFIG["__CUCU_AFTER_STEP_HOOKS"] = []
 
     CONFIG["__CUCU_PAGE_CHECK_HOOKS"] = {}
+
     CONFIG["CUCU_BROKEN_IMAGES_PAGE_CHECK"] = "enabled"
     CONFIG["CUCU_READY_STATE_PAGE_CHECK"] = "enabled"
 
 
-def register_before_all_scenario_hook(hook_func):
+def init_scenario_hook_variables():
+    CONFIG["__CUCU_BEFORE_THIS_SCENARIO_HOOKS"] = []
+    CONFIG["__CUCU_AFTER_THIS_SCENARIO_HOOKS"] = []
+
+
+def register_before_scenario_hook(hook_func):
     """
     register a before scenario hook that will execute before eery scenario has
     executed and we will pass the current behave context object as the only
@@ -29,22 +33,13 @@ def register_before_all_scenario_hook(hook_func):
     CONFIG["__CUCU_BEFORE_SCENARIO_HOOKS"].append(hook_func)
 
 
-def register_after_all_scenario_hook(hook_func):
+def register_after_scenario_hook(hook_func):
     """
     register an after scenario hook that will execute after every scenario has
     executed and we will pass the current behave context object as the only
     argument to the hook_func provided.
     """
     CONFIG["__CUCU_AFTER_SCENARIO_HOOKS"].append(hook_func)
-
-
-def register_before_this_scenario_hook(hook_func):
-    """
-    register a before scenario hook that will only once before the next scenario
-    has executed and we will pass the current behave context object as the only
-    argument to the hook_func provided.
-    """
-    CONFIG["__CUCU_BEFORE_THIS_SCENARIO_HOOKS"].append(hook_func)
 
 
 def register_after_this_scenario_hook(hook_func):
@@ -54,6 +49,24 @@ def register_after_this_scenario_hook(hook_func):
     argument to the hook_func provided.
     """
     CONFIG["__CUCU_AFTER_THIS_SCENARIO_HOOKS"].append(hook_func)
+
+
+def register_before_step_hook(hook_func):
+    """
+    register a before step hook that will execute before every step is executed
+    and we will pass the current behave context object as the only argument to
+    the hook_func provided.
+    """
+    CONFIG["__CUCU_BEFORE_STEP_HOOKS"].append(hook_func)
+
+
+def register_after_step_hook(hook_func):
+    """
+    register an after step hook that will execute after every step executes and
+    we will pass the current behave context object as the only argument to the
+    hook_func provided.
+    """
+    CONFIG["__CUCU_AFTER_STEP_HOOKS"].append(hook_func)
 
 
 def register_page_check_hook(name, hook_func):
