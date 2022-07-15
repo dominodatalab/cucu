c49b5c52a0e1e0f00bad7f4984b050e42d5c6f67
Daniel <daniel.burton@dominodatalab.com>
2022-07-15 01:40:42 +0000

    [QE-7581] Update image steps to use fuzzy

--- src/cucu/steps/image_steps.py
+++ src/cucu/steps/image_steps.py
@@ -1,4 +1,4 @@
-from cucu import helpers, retry, step
+from cucu import helpers, retry, step, fuzzy
 
 
 def find_image(ctx, name, index=0):
@@ -16,14 +16,13 @@ def find_image(ctx, name, index=0):
     returns:
         the WebElement that matches the provided arguments.
     """
-    image = None
-    name = name.replace('"', '\\"')
-    imgs = ctx.browser.css_find_elements(f'img[alt="{name}"')
-
-    if len(imgs) > index:
-        image = imgs[index]
-
-    return image
+    return fuzzy.find(
+        ctx.browser,
+        name,
+        ["img"],
+        index=index,
+        attributes=["alt"]
+    )
 
 
 helpers.define_should_see_thing_with_name_steps(
