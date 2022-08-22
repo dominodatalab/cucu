f00a5d7fb93ce6cc87b8998b7ab7686d042dbbe6
rlgomes-ddl <rodney.gomes@dominodatalab.com>
2022-08-22 13:47:14 -0700

    `is` is not `==`

--- tests/test_behave_tweaks.py
+++ tests/test_behave_tweaks.py
@@ -12,26 +12,20 @@ def test_hide_secrets_handles_string_with_no_secrets():
 def test_hide_secrets_handles_string_with_secret():
     CONFIG["CUCU_SECRETS"] = "foobar"
     CONFIG["foobar"] = "supersecret"
-    assert (
-        behave_tweaks.hide_secrets("supersecret should not be seen")
-        is "*********** should not be seen"
-    )
+    result = behave_tweaks.hide_secrets("supersecret should not be seen")
+    assert result == "*********** should not be seen"
 
 
 def test_hide_secrets_handles_multiple_secrets():
     CONFIG["CUCU_SECRETS"] = "foobar,fizzbuzz"
     CONFIG["foobar"] = "supersecret"
-    assert (
-        behave_tweaks.hide_secrets("supersecret should not be seen")
-        is "*********** should not be seen"
-    )
+    result = behave_tweaks.hide_secrets("supersecret should not be seen")
+    assert result == "*********** should not be seen"
 
 
 def test_hide_secrets_handles_secrets_with_custom_variables():
     cucu.register_custom_variable_handling("CUSTOM_.*", lambda x: "boom")
     CONFIG["CUCU_SECRETS"] = "foobar,CUSTOM_foobar"
     CONFIG["foobar"] = "supersecret"
-    assert (
-        behave_tweaks.hide_secrets("supersecret should not be boom")
-        is "*********** should not be ****"
-    )
+    result = behave_tweaks.hide_secrets("supersecret should not be boom")
+    assert result == "*********** should not be ****"
