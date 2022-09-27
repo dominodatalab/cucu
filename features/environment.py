521240df62d1886310a0eefcf09195c85ebab41f
rlgomes-ddl <rodney.gomes@dominodatalab.com>
2022-09-27 08:24:13 -0700

    cleaner solution

--- features/environment.py
+++ features/environment.py
@@ -4,6 +4,7 @@ import urllib
 from cucu import register_custom_junit_failure_handler
 from cucu.config import CONFIG
 from cucu.environment import *
+from textwrap import dedent
 
 
 def circle_ci_direct_links_to_reports(feature, scenario):
@@ -11,15 +12,20 @@ def circle_ci_direct_links_to_reports(feature, scenario):
     custom failure handler that will add links from the tests Tab in CircleCI to
     link directly to our HTML Test Report
     """
-    build_url = CONFIG["CIRCLE_BUILD_URL"]
-    feature_name = urllib.parse.quote(feature.name)
-    scenario_name = urllib.parse.quote(scenario.name)
-    if build_url is not None:
-        job_id = CONFIG["CIRCLE_WORKFLOW_JOB_ID"]
-        return f"""
-See the HTML Test Report for more details(triple click the link, then copy and paste into your browser):
-https://output.circle-artifacts.com/output/job/{job_id}/artifacts/0/report/{feature_name}/{scenario_name}/index.html
+    job_id = CONFIG["CIRCLE_WORKFLOW_JOB_ID"]
+
+    if job_id is not None:
+        feature_name = urllib.parse.quote(feature.name)
+        scenario_name = urllib.parse.quote(scenario.name)
+
+        return dedent(
+            f"""
+        See the HTML Test Report for more details(triple click the link, then copy and paste into your browser):
+        https://output.circle-artifacts.com/output/job/{job_id}/artifacts/0/report/{feature_name}/{scenario_name}/index.html
         """
+        )
+
+    return ""
 
 
 register_custom_junit_failure_handler(circle_ci_direct_links_to_reports)
