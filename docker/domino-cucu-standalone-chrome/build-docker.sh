857e90f3203bc9befea44e06ad87d75599dae116
Daniel Burton <daniel.burton@daniel.burton>
2022-10-14 16:03:56 -0400

    [QE-9244] Got Dockerfile working a bit better (but Python build still fails)

---
+++ docker/domino-cucu-standalone-chrome/build-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+script_path="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
+project_path="$(readlink -f ${script_path}/../..)"
+python_version="$(cat $project_path/.python-version)"
+docker build -t domino-cucu-standalone-chrome --build-arg PYTHON_VERSION=${python_version} .
