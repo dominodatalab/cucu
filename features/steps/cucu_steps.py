b5ab77b95c721b37d678c2c0c949c6142b2ac501
rlgomes-ddl <98488861+rlgomes-ddl@users.noreply.github.com>
2022-11-02 12:24:01 -0700

    fix frame switching code (#248)
    * fix frame switching code
    
    * there was a bug when starting from an inner frame context since we
      never switched back to the default frame in order to pick up those
      elements
    
    * Update features/cli/internals.feature
    
    Co-authored-by: Xin Dong <104880864+ddl-xin@users.noreply.github.com>
    
    * tagged release 0.95.0
    
    Co-authored-by: Xin Dong <104880864+ddl-xin@users.noreply.github.com>

--- features/steps/cucu_steps.py
+++ features/steps/cucu_steps.py
@@ -7,6 +7,12 @@ from selenium.webdriver.common.by import By
 
 @step('I should see the element with id "{id}" has a child')
 def should_see_the_element_with_id_has_a_child(ctx, id):
-    ctx.check_browser_initialized()
+    # this code forces the frame switching code to switch out of the wrong
+    # frame when looking for elements that happen to be in the default
+    # (top level) frame
+    frames = ctx.browser.execute('return document.querySelectorAll("iframe");')
+    if frames:
+        ctx.browser.switch_to_frame(frames[-1])
+
     element = ctx.browser.css_find_elements(f"*[id={id}]")[0]
-    child = element.find_elements(By.CSS_SELECTOR, "*")[0]
+    element.find_elements(By.CSS_SELECTOR, "*")[0]
