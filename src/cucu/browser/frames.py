6da2f69d6ec2cf1562634169ac7a5c5ef7f8bd38
Cedric Young <cedric.young@dominodatalab.com>
2022-12-02 18:28:48 +0000

    unfurl logic

--- src/cucu/browser/frames.py
+++ src/cucu/browser/frames.py
@@ -13,27 +13,27 @@ def search_in_all_frames(browser, search_function):
     """
     result = search_function()
 
-    if not result:
-        # switch to default frame and check for the desired element before
-        # proceeding to search for it in the available frames
-        browser.switch_to_default_frame()
+    if result:
+        return
 
+    # switch to default frame and check for the desired element before
+    # proceeding to search for it in the available frames
+    browser.switch_to_default_frame()
+
+    result = search_function()
+    if result:
+        return result
+
+    frames = browser.execute('return document.querySelectorAll("iframe");')
+    for frame in frames:
+        #
+        # need to switch back to the default frame before switching into any
+        # other inner frame otherwise the context will be wrong and we'd
+        # generate a stale element exception
+        #
+        browser.switch_to_default_frame()
+        browser.switch_to_frame(frame)
         result = search_function()
+
         if result:
             return result
-
-        frames = browser.execute('return document.querySelectorAll("iframe");')
-        for frame in frames:
-            #
-            # need to switch back to the default frame before switching into any
-            # other inner frame otherwise the context will be wrong and we'd
-            # generate a stale element exception
-            #
-            browser.switch_to_default_frame()
-            browser.switch_to_frame(frame)
-            result = search_function()
-
-            if result:
-                return result
-
-    return result
