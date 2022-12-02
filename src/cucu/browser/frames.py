87bde86e0e7931d1e99b8f8f6a5c316dfbd9e1da
Cedric Young <cedric.young@dominodatalab.com>
2022-12-02 19:51:11 +0000

    update comments

--- src/cucu/browser/frames.py
+++ src/cucu/browser/frames.py
@@ -5,33 +5,35 @@ def search_in_all_frames(browser, search_function):
     """
     search all frames on the page for an element
 
+    Warning: This leaves the browser in whatever frame was last searched so that
+    users of this method are in that frame.
+
     parameters:
       browser           - the cucu.browser.Browser object
       search_function   - function to search for the element (within a frame)
     returns:
         the WebElement that matches (if found)
     """
+    # check whatever frame we're currently in
     result = search_function()
+    if result:
+        return
 
-    if not result:
-        # switch to default frame and check for the desired element before
-        # proceeding to search for it in the available frames
+    # we might have not been in the default frame so check again
+    browser.switch_to_default_frame()
+    # logger.debug("Switched browser to the default frame")
+    result = search_function()
+    if result:
+        return result
+
+    frames = browser.execute('return document.querySelectorAll("iframe");')
+    for frame in frames:
+        # need to be in the default frame in order to switch to a child
+        # frame w/o getting a stale element exception
         browser.switch_to_default_frame()
-
+        browser.switch_to_frame(frame)
         result = search_function()
+
         if result:
+            # logger.debug("Switched browser to an iframe")
             return result
-
-        frames = browser.execute('return document.querySelectorAll("iframe");')
-        for frame in frames:
-            # need to switch back to the default frame before switching into any
-            # other inner frame otherwise the context will be wrong and we'd
-            # generate a stale element exception
-            browser.switch_to_default_frame()
-            browser.switch_to_frame(frame)
-            result = search_function()
-
-            if result:
-                return result
-
-    return result
