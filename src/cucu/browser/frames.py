e75ae65e8cbaf5886b77ba3b2a4b2e983f086ccc
Cedric Young <cedric.young@dominodatalab.com>
2022-12-02 20:01:27 +0000

    revert to furled logic

--- src/cucu/browser/frames.py
+++ src/cucu/browser/frames.py
@@ -16,24 +16,26 @@ def search_in_all_frames(browser, search_function):
     """
     # check whatever frame we're currently in
     result = search_function()
-    if result:
-        return
 
-    # we might have not been in the default frame so check again
-    browser.switch_to_default_frame()
-    # logger.debug("Switched browser to the default frame")
-    result = search_function()
-    if result:
-        return result
-
-    frames = browser.execute('return document.querySelectorAll("iframe");')
-    for frame in frames:
-        # need to be in the default frame in order to switch to a child
-        # frame w/o getting a stale element exception
+    if not result:
+        # we might have not been in the default frame so check agai
         browser.switch_to_default_frame()
-        browser.switch_to_frame(frame)
-        result = search_function()
+        logger.debug("Switched browser to the default frame")
 
+        result = search_function()
         if result:
-            # logger.debug("Switched browser to an iframe")
             return result
+
+        frames = browser.execute('return document.querySelectorAll("iframe");')
+        for frame in frames:
+            # need to be in the default frame in order to switch to a child
+            # frame w/o getting a stale element exception
+            browser.switch_to_default_frame()
+            browser.switch_to_frame(frame)
+            result = search_function()
+
+            if result:
+                logger.debug("Switched browser to an iframe")
+                return result
+
+    return result
