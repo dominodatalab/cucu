7ee0413f0966a18cc83faa3cfdeea03472b8bc68
rlgomes-ddl <98488861+rlgomes-ddl@users.noreply.github.com>
2022-12-06 10:17:21 -0800

    quick fix for a dev script (#280)

--- bin/start_selenium_hub.sh
+++ bin/start_selenium_hub.sh
@@ -5,10 +5,11 @@ docker kill selenium_hub selenium_node_chrome selenium_node_firefox selenium_nod
 docker rm selenium_hub selenium_node_chrome selenium_node_firefox selenium_node_edge || true > /dev/null 2>&1
 
 NODE_OPTS="--link selenium_hub
-           -e SE_NODE_MAX_SESSIONS=10
-           -e SE_EVENT_BUS_HOST=selenium_hub
-           -e SE_EVENT_BUS_PUBLISH_PORT=4442
-           -e SE_EVENT_BUS_SUBSCRIBE_PORT=4443"
+           --env SE_NODE_OVERRIDE_MAX_SESSIONS=true
+           --env SE_NODE_MAX_SESSIONS=10
+           --env SE_EVENT_BUS_HOST=selenium_hub
+           --env SE_EVENT_BUS_PUBLISH_PORT=4442
+           --env SE_EVENT_BUS_SUBSCRIBE_PORT=4443"
 
 docker run -d -p 4444:4444 --name selenium_hub selenium/hub:latest
 docker run -d $NODE_OPTS --name selenium_node_chrome selenium/node-chrome:latest
