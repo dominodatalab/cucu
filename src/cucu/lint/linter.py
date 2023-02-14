c62198979e7bf92be9f92e35c57a360030d8a47d
Cedric Young <Cedric.Young@dominodatalab.com>
2023-02-13 16:07:26 -0800

    simplify lint messaging

--- src/cucu/lint/linter.py
+++ src/cucu/lint/linter.py
@@ -104,7 +104,7 @@ def parse_matcher(name, rule_name, rule, line, state):
                 if other_filepath != feature_filepath:
                     return (
                         True,
-                        f', "{value}" used in "{feature_filepath}" and "{other_filepath}"',
+                        f', "{value}" also used in "{other_filepath}"',
                     )
 
             state["unique_per_all_features"][rule_name][
@@ -125,40 +125,19 @@ def parse_matcher(name, rule_name, rule, line, state):
 
             if value in state["unique_per_all_scenarios"][rule_name]:
                 # we have another scenario which already has this value in use.
-                other_scenario_name = state["unique_per_all_scenarios"][
-                    rule_name
-                ][value][0]
+                other_file_path, other_line_number, other_scenario_name = state[
+                    "unique_per_all_scenarios"
+                ][rule_name][value]
 
-                other_file_path = state["unique_per_all_scenarios"][rule_name][
-                    value
-                ][1]
-
-                if other_file_path == feature_filepath:
-                    if scenario_name == other_scenario_name:
-                        return (
-                            True,
-                            f', "{value}" duplicated in "{feature_filepath}"',
-                        )
-                    else:
-                        return (
-                            True,
-                            f', "{value}" used in "{feature_filepath}" scenario name:"{scenario_name}" and scenario name:"{other_scenario_name}"',
-                        )
-                else:
-                    if scenario_name == other_scenario_name:
-                        return (
-                            True,
-                            f', "{value}" used in "{feature_filepath}" and "{other_file_path}"',
-                        )
-                    else:
-                        return (
-                            True,
-                            f', "{value}" used in "{feature_filepath}" scenario name:"{scenario_name}" and "{other_file_path}" scenario name:"{other_scenario_name}"',
-                        )
+                return (
+                    True,
+                    f', "{value}" also used in "{other_file_path}:{other_line_number}" Scenario: "{other_scenario_name}"',
+                )
 
             state["unique_per_all_scenarios"][rule_name][value] = [
-                scenario_name,
                 feature_filepath,
+                state["current_line_number"],
+                scenario_name,
             ]
 
             return (False, "")
@@ -387,6 +366,8 @@ def lint(filepath):
         }
 
         for line in lines:
+            state["current_line_number"] = line_number
+
             feature_match = re.match(".*Feature: (.*)", line)
             if feature_match is not None:
                 state["current_feature_name"] = feature_match.group(1)
