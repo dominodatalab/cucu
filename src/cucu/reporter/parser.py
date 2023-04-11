99a93766ffcddc4e7c39280031734f3061a4e24e
Cedric Young <Cedric.Young@dominodatalab.com>
2023-04-10 17:16:25 -0700

    log unknown ansi codes

--- src/cucu/reporter/parser.py
+++ src/cucu/reporter/parser.py
@@ -1,12 +1,15 @@
 import html
 import re
 
+from cucu import logger
+
 ESC_SEQ = "["
 TRANSLATION = {
     ESC_SEQ + "1A": "",  # throw away "move cursor up 1"
     ESC_SEQ + "0m": "</span>",
     ESC_SEQ + "31m": '<span style="color: red;">',
     ESC_SEQ + "32m": '<span style="color: green;">',
+    ESC_SEQ + "33m": '<span style="color: yellow;">',
     ESC_SEQ + "35m": '<span style="color: magenta">',
     ESC_SEQ + "90m": '<span style="color: darkgrey;">',
 }
@@ -17,4 +20,8 @@ def parse_log_to_html(input: str) -> str:
     """ "
     Parse an ansi color log to html
     """
-    return f"<pre>\n{REGEX.sub(lambda match: TRANSLATION[match.group(0)], html.escape(input, quote=False))}\n</pre>\n"
+    result = f"<pre>\n{REGEX.sub(lambda match: TRANSLATION[match.group(0)], html.escape(input, quote=False))}\n</pre>\n"
+    if ESC_SEQ in result:
+        logger.warn("Detected unmapped ansi escape code!")
+
+    return result
