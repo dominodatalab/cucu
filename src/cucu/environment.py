a540c43a7728269aa6e4faf4a57ed06f27a51459
Cedric Young <Cedric.Young@dominodatalab.com>
2023-06-29 10:08:03 -0700

    fix var name

--- src/cucu/environment.py
+++ src/cucu/environment.py
@@ -133,12 +133,12 @@ def after_scenario(ctx, scenario):
     for timer_name in ctx.step_timers:
         logger.warn(f'timer "{timer_name}" was never stopped/recorded')
 
-    if ctx.browsers and config.CONFIG["CUCU_BROWSER"].lower() == "chrome":
+    if ctx.browsers and len(ctx.browsers) > 0 and config.CONFIG["CUCU_BROWSER"].lower() == "chrome":
         mht_filename = os.path.join(
             CONFIG["SCENARIO_LOGS_DIR"],
             "browser_snapshot.mht",
         )
-        browser[0].download_mht(mht_filename)
+        ctx.browsers[0].download_mht(mht_filename)
 
     # run after all scenario hooks
     for hook in CONFIG["__CUCU_AFTER_SCENARIO_HOOKS"]:
