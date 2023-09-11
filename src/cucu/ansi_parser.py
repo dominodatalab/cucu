96974981f770e6e8a445505285ca21921e01e61b
Xin Dong <xin.dong@dominodatalab.com>
2023-09-11 09:49:49 -0700

    QE-13285 improve colorized log html file

--- src/cucu/ansi_parser.py
+++ src/cucu/ansi_parser.py
@@ -4,20 +4,29 @@ import re
 from behave.formatter.ansi_escapes import colors, escapes
 
 ESC_SEQ = r"\x1b["
-TRANSLATION = {v: f'<span style="color: {k};">' for k, v in colors.items()} | {
-    escapes["reset"]: "</span>",
-    escapes["up"]: "",  # ignore away move cursor up 1
-    ESC_SEQ + "46": "",  # ignore DELETE (num keypad)
-    ESC_SEQ + "48": "",  # ignore INSERT (num keypad)
-    ESC_SEQ + "49": "",  # ignore END (num keypad)
-    ESC_SEQ + "50": "",  # ignore DOWN ARROW (num keypad)
-    ESC_SEQ + "51": "",  # ignore PAGE DOWN (num keypad)
-    ESC_SEQ + "52": "",  # ignore LEFT ARROW (num keypad)
-    ESC_SEQ + "54": "",  # ignore RIGHT ARROW (num keypad)
-    ESC_SEQ + "55": "",  # ignore HOME (num keypad)
-    ESC_SEQ + "56": "",  # ignore UP ARROW (num keypad)
-    ESC_SEQ + "57": "",  # ignore PAGE UP (num keypad)
-}
+TRANSLATION = (
+    {colors["grey"]: '<span style="color: grey;">\n'}
+    | {  # grey indicates a new step, and hence add a line break
+        v: f'<span style="color: {k};">'
+        for k, v in colors.items()
+        if k != "grey"
+    }
+    | {
+        "\n" + escapes["reset"] + "\n": "</span>\n",
+        escapes["reset"]: "</span>",
+        escapes["up"] + "\n": "",  # remove a line break with cursor up
+        ESC_SEQ + "46": "",  # ignore DELETE (num keypad)
+        ESC_SEQ + "48": "",  # ignore INSERT (num keypad)
+        ESC_SEQ + "49": "",  # ignore END (num keypad)
+        ESC_SEQ + "50": "",  # ignore DOWN ARROW (num keypad)
+        ESC_SEQ + "51": "",  # ignore PAGE DOWN (num keypad)
+        ESC_SEQ + "52": "",  # ignore LEFT ARROW (num keypad)
+        ESC_SEQ + "54": "",  # ignore RIGHT ARROW (num keypad)
+        ESC_SEQ + "55": "",  # ignore HOME (num keypad)
+        ESC_SEQ + "56": "",  # ignore UP ARROW (num keypad)
+        ESC_SEQ + "57": "",  # ignore PAGE UP (num keypad)
+    }
+)
 RE_TO_HTML = re.compile("|".join(map(re.escape, TRANSLATION)))
 
 RE_TO_REMOVE = re.compile(
@@ -33,10 +42,12 @@ def parse_log_to_html(input: str) -> str:
     """
     Parse an ansi color log to html
     """
-
-    body_start = '<body style="color: white; background-color: 333;">'  # use dark bg since colors are from behave
+    html_start = "<!DOCTYPE html>\n<html>\n"
+    html_end = "</html>\n"
+    head = '<head>\n<meta http-equiv="Content-Type" content="text/html; charset=utf-8">\n</head>\n'
+    body_start = '<body style="color: white; background-color: #100c08;">'  # use dark bg since colors are from behave
     body_end = "</body>\n"
-    result = f"{body_start}<pre>\n{RE_TO_HTML.sub(lambda match: TRANSLATION[match.group(0)], html.escape(input, quote=False))}\n</pre>{body_end}"
+    result = f"{html_start}{head}{body_start}<pre>\n{RE_TO_HTML.sub(lambda match: TRANSLATION[match.group(0)], html.escape(input, quote=False))}\n</pre>{body_end}{html_end}"
     if ESC_SEQ in result:
         lines = "\n".join([x for x in result.split("\n") if ESC_SEQ in x])
 
