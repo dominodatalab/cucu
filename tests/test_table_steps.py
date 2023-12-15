8933310d19e1e8436df8984aaa7619cbfda868ae
Carlin Fuerst <carlin.fuerst@dominodatalab.com>
2023-12-15 09:08:42 -0800

    Condense a bit

--- tests/test_table_steps.py
+++ tests/test_table_steps.py
@@ -7,6 +7,20 @@ from cucu.steps.table_steps import (
 
 
 class TestDoNotFindTable(TestCase):
+
+    def __init__(self, method_name='runTest'):
+        super().__init__(method_name)
+        self.seen_tables = [
+            [
+                ["", "", "✕"]
+            ],
+            [
+                ["", "NAME", "SIZE", "MODIFIED", ""],
+                ["", "", "", "", ""],
+                ["", "3GB_file.txt", "3.2 GB", "Dec 14, 2023 8:46 PM", ""],
+            ],
+        ]
+
     def test_do_not_find_table_error(self):
         # Assert that if ANY table matches the undesired table pattern, a RuntimeError is raised
         undesired_table = [
@@ -17,16 +31,8 @@ class TestDoNotFindTable(TestCase):
         with mock.patch(
             "cucu.steps.table_steps.behave_table_to_array", expected_mock
         ):
-            tables = [
-                [["", "", "✕"]],
-                [
-                    ["", "NAME", "SIZE", "MODIFIED", ""],
-                    ["", "", "", "", ""],
-                    ["", "3GB_file.txt", "3.2 GB", "Dec 14, 2023 8:46 PM", ""],
-                ],
-            ]
-            tables_mock = mock.MagicMock(return_value=tables)
-            with mock.patch("cucu.steps.table_steps.find_tables", tables_mock):
+            seen_mock = mock.MagicMock(return_value=self.seen_tables)
+            with mock.patch("cucu.steps.table_steps.find_tables", seen_mock):
                 dummy = mock.MagicMock()
                 with self.assertRaises(RuntimeError):
                     do_not_find_table(
@@ -43,16 +49,8 @@ class TestDoNotFindTable(TestCase):
         with mock.patch(
             "cucu.steps.table_steps.behave_table_to_array", expected_mock
         ):
-            tables = [
-                [["", "", "✕"]],
-                [
-                    ["", "NAME", "SIZE", "MODIFIED", ""],
-                    ["", "", "", "", ""],
-                    ["", "3GB_file.txt", "3.2 GB", "Dec 14, 2023 8:46 PM", ""],
-                ],
-            ]
-            tables_mock = mock.MagicMock(return_value=tables)
-            with mock.patch("cucu.steps.table_steps.find_tables", tables_mock):
+            seen_mock = mock.MagicMock(return_value=self.seen_tables)
+            with mock.patch("cucu.steps.table_steps.find_tables", seen_mock):
                 dummy = mock.MagicMock()
                 result = do_not_find_table(
                     dummy, check_table_contains_matching_rows_in_table
