dominodatalab/cucu

commit 45f9fff85cecc3f29ad80af506609bcd0abdf263

QE-11488 update filenames and paths (#442)
Issue: [QE-11488](https://dominodatalab.atlassian.net/browse/QE-11488)
Solution: Shorten filenames and remove quotes

[QE-11488]:
https://dominodatalab.atlassian.net/browse/QE-11488?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "cucu"
-version = "0.190.0"
+version = "0.191.0"
 license = "MIT"
 description = "Easy BDD web testing"
 authors = ["Domino Data Lab <open-source@dominodatalab.com>"]

src/cucu/reporter/html.py
@@ -14,7 +14,7 @@ import jinja2
 from cucu import format_gherkin_table, logger
 from cucu.ansi_parser import parse_log_to_html
 from cucu.config import CONFIG
-from cucu.utils import get_step_image_dir
+from cucu.utils import ellipsize_filename, get_step_image_dir
 
 
 def escape(data):
@@ -123,8 +123,12 @@ def generate(results, basepath, only_failures=False):
 
         if feature["status"] not in ["skipped", "untested"]:
             # copy each feature directories contents over to the report directory
-            src_feature_filepath = os.path.join(results, feature["name"])
-            dst_feature_filepath = os.path.join(basepath, feature["name"])
+            src_feature_filepath = os.path.join(
+                results, ellipsize_filename(feature["name"])
+            )
+            dst_feature_filepath = os.path.join(
+                basepath, ellipsize_filename(feature["name"])
+            )
             shutil.copytree(
                 src_feature_filepath, dst_feature_filepath, dirs_exist_ok=True
             )
@@ -133,7 +137,9 @@ def generate(results, basepath, only_failures=False):
             CONFIG.restore()
 
             scenario_filepath = os.path.join(
-                basepath, feature["name"], scenario["name"]
+                basepath,
+                ellipsize_filename(feature["name"]),
+                ellipsize_filename(scenario["name"]),
             )
 
             scenario_configpath = os.path.join(

src/cucu/utils.py
@@ -157,19 +157,31 @@ def text_in_current_frame(browser: Browser) -> str:
 
 
 def ellipsize_filename(raw_filename):
-    max_filename = 255
-    if len(raw_filename) < max_filename:
-        return raw_filename
+    max_filename = 100
+    new_raw_filename = normalize_filename(raw_filename)
+    if len(new_raw_filename) < max_filename:
+        return new_raw_filename
 
     ellipsis = "..."
     # save the last chars, as the ending is often important
-    end_count = 100
+    end_count = 40
     front_count = max_filename - (len(ellipsis) + end_count)
     ellipsized_filename = (
-        raw_filename[:front_count] + ellipsis + raw_filename[-1 * end_count :]
+        new_raw_filename[:front_count]
+        + ellipsis
+        + new_raw_filename[-1 * end_count :]
     )
-
     return ellipsized_filename
+
+
+def normalize_filename(raw_filename):
+    normalized_filename = (
+        raw_filename.replace('"', "")
+        .replace("{", "")
+        .replace("}", "")
+        .replace("#", "")
+    )
+    return normalized_filename
 
 
 def get_step_image_dir(step_index, step_name):

features/browser/images.feature
@@ -14,4 +14,4 @@ Feature: Images
      When I click the button "I should see the button "button with child""
      Then I should not see the image with the alt text "After I open a browser at the url "http://{HOST_ADDRESS}:{PORT}/buttons.html""
       And I should not see the image with the alt text "After I open a browser at the url "http://{HOST_ADDRESS}:{PORT}/buttons.html""
-      And I should see the image with the alt text "After I should see the button "button with child""
+      And I should see the image with the alt text "After I should see the button button with child"

features/cli/report_basics.feature
@@ -50,7 +50,7 @@ Feature: Report basics
 
         * # Can see image for step with secret in name
      When I click the button "I should see the text "\{MY_SECRET\}""
-     Then I should see the image with the alt text "After I should see the text "\{MY_SECRET\}""
+     Then I should see the image with the alt text "After I should see the text MY_SECRET"
 
         * # Cannot see secrets in the exception message
      When I click the button "Then I click the button "\{MY_SECRET\}""
@@ -70,13 +70,13 @@ Feature: Report basics
       And I should not see the image with the alt text "After I open a browser at the url \"http://{HOST_ADDRESS}:{PORT}/checkboxes.html\""
       And I should not see the image with the alt text "After I should see the checkbox \"checkbox with inner label\""
      Then I click the button "Then I should see the checkbox \"checkbox with inner label\""
-      And I should see the image with the alt text "After I should see the checkbox \"checkbox with inner label\""
+      And I should see the image with the alt text "After I should see the checkbox checkbox with inner label"
       And I should not see the image with the alt text "After I start a webserver at directory \"data/www\" and save the port to the variable \"PORT\""
       And I should not see the image with the alt text "After I open a browser at the url \"http://{HOST_ADDRESS}:{PORT}/checkboxes.html""
      When I save the current url to the variable "CURRENT_URL"
       And I click the link "Index"
      Then I navigate to the url "{CURRENT_URL}"
-      And I wait to see the image with the alt text "After I should see the checkbox \"checkbox with inner label\""
+      And I wait to see the image with the alt text "After I should see the checkbox checkbox with inner label"
       And I should not see the image with the alt text "After I start a webserver at directory \"data/www\" and save the port to the variable \"PORT\""
       And I should not see the image with the alt text "After I open a browser at the url \"http://{HOST_ADDRESS}:{PORT}/checkboxes.html\""
 
@@ -322,4 +322,4 @@ Feature: Report basics
        | .*     | Just a scenario that opens a web page | 3     | failed | .*       |
      When I click the button "Just a scenario that opens a web page"
       And I wait to click the button "show images"
-      And I should see the image with the alt text "After I should see the text \"inexistent\""
+      And I should see the image with the alt text "After I should see the text inexistent"

features/cli/secrets.feature
@@ -144,7 +144,7 @@ Feature: Secrets
            Then I pass the secret "\{MY_SECRET\}" to a substep
       """
      When I run the command "cucu run {CUCU_RESULTS_DIR}/substeps_without_variable_passthru --secrets MY_SECRET --results={CUCU_RESULTS_DIR}/substeps_without_variable_passthru_results" and save stdout to "STDOUT", stderr to "STDERR" and expect exit code "0"
-     Then I should see a file at "{CUCU_RESULTS_DIR}/substeps_without_variable_passthru_results/Feature that spills the beans/This scenario prints some secrets to the logs/0003 - I echo "************"/0000 - After I echo "************".png"
+     Then I should see a file at "{CUCU_RESULTS_DIR}/substeps_without_variable_passthru_results/Feature that spills the beans/This scenario prints some secrets to the logs/0003 - I echo ************/0000 - After I echo ************.png"
 
   Scenario: User gets expected behavior when using variable_passthru=True
     Given I create a file at "{CUCU_RESULTS_DIR}/substeps_with_variable_passthru/environment.py" with the following:
@@ -178,4 +178,4 @@ Feature: Secrets
            Then I pass the secret "\{MY_SECRET\}" to a substep
       """
      When I run the command "cucu run {CUCU_RESULTS_DIR}/substeps_with_variable_passthru --secrets MY_SECRET --results={CUCU_RESULTS_DIR}/substeps_with_variable_passthru_results" and save stdout to "STDOUT", stderr to "STDERR" and expect exit code "0"
-     Then I should see a file at "{CUCU_RESULTS_DIR}/substeps_with_variable_passthru_results/Feature that spills the beans/This scenario prints some secrets to the logs/0003 - I echo "\{MY_SECRET\}"/0000 - After I echo "\{MY_SECRET\}".png"
+     Then I should see a file at "{CUCU_RESULTS_DIR}/substeps_with_variable_passthru_results/Feature that spills the beans/This scenario prints some secrets to the logs/0003 - I echo MY_SECRET/0000 - After I echo MY_SECRET.png"

src/cucu/environment.py
@@ -10,7 +10,7 @@ from functools import partial
 from cucu import config, init_scenario_hook_variables, logger
 from cucu.config import CONFIG
 from cucu.page_checks import init_page_checks
-from cucu.utils import take_screenshot
+from cucu.utils import ellipsize_filename, take_screenshot
 
 CONFIG.define(
     "FEATURE_RESULTS_DIR",
@@ -57,7 +57,9 @@ def after_all(ctx):
 def before_feature(ctx, feature):
     if config.CONFIG["CUCU_RESULTS_DIR"] is not None:
         results_dir = config.CONFIG["CUCU_RESULTS_DIR"]
-        ctx.feature_dir = os.path.join(results_dir, feature.name)
+        ctx.feature_dir = os.path.join(
+            results_dir, ellipsize_filename(feature.name)
+        )
         CONFIG["FEATURE_RESULTS_DIR"] = ctx.feature_dir
 
 
@@ -86,7 +88,9 @@ def before_scenario(ctx, scenario):
     ctx.step_timers = {}
 
     if config.CONFIG["CUCU_RESULTS_DIR"] is not None:
-        ctx.scenario_dir = os.path.join(ctx.feature_dir, scenario.name)
+        ctx.scenario_dir = os.path.join(
+            ctx.feature_dir, ellipsize_filename(scenario.name)
+        )
         CONFIG["SCENARIO_RESULTS_DIR"] = ctx.scenario_dir
         os.makedirs(ctx.scenario_dir, exist_ok=True)
 

commit 47c3daed39578e00c31ec59739b37c1548ed6158

QE-15340 QE-15386 Fix report of long feature/scenario names and replace & in file names (#450)
There is a bug introduced in
https://github.com/cerebrotech/cucu/pull/442 such that if the
feature/scenario names are long, the report is not generated in the
right folder.

This PR fixes that bug and created a test to verify it. It also save the
shortened name in the Junit report such that it can be used by
`domino-testrail` to generate the link of the html report on TestRail

In addition, `&` is replaced in the file names

data/features/feature_with_long_names.feature
@@ -1,0 +1,5 @@
+Feature: Feature with an absolutely unnecessarily, ridiculously, comically, I don't know what they're thinking, long name
+
+  Scenario: Scenario with an absolutely unnecessarily, ridiculously, comically, I don't know what they're thinking, long name
+    Given I start a webserver at directory "data/www" and save the port to the variable "PORT"
+      And I open a browser at the url "http://{HOST_ADDRESS}:{PORT}/buttons.html"

features/cli/run_outputs.feature
@@ -144,26 +144,26 @@ Feature: Run outputs
      Then I should see a file at "{CUCU_RESULTS_DIR}/validate_junit_xml_results/Feature with mixed results.xml"
       And I should see the file at "{CUCU_RESULTS_DIR}/validate_junit_xml_results/Feature with mixed results.xml" matches the following:
       """
-      <testsuite name="Feature with mixed results" tests="7" errors="0" failures="3" skipped="1" timestamp=".*" tags="mixed">
-       <testcase classname="Feature with mixed results" name="Scenario that passes" status="passed" time=".*">
+      <testsuite name="Feature with mixed results" foldername="Feature with mixed results" tests="7" errors="0" failures="3" skipped="1" timestamp=".*" tags="mixed">
+       <testcase classname="Feature with mixed results" name="Scenario that passes" foldername="Scenario that passes" status="passed" time=".*">
        </testcase>
-       <testcase classname="Feature with mixed results" name="Scenario that fails" status="failed" time=".*">
+       <testcase classname="Feature with mixed results" name="Scenario that fails" foldername="Scenario that fails" status="failed" time=".*">
         <failure>
         [\s\S]*
         </failure>
        </testcase>
-       <testcase classname="Feature with mixed results" name="Scenario and after-hook both fail" status="failed" time=".*">
+       <testcase classname="Feature with mixed results" name="Scenario and after-hook both fail" foldername="Scenario and after-hook both fail" status="failed" time=".*">
         <failure>
         [\s\S]*
         </failure>
        </testcase>
-       <testcase classname="Feature with mixed results" name="Scenario with after-hook error" status="passed" time=".*">
+       <testcase classname="Feature with mixed results" name="Scenario with after-hook error" foldername="Scenario with after-hook error" status="passed" time=".*">
        </testcase>
-       <testcase classname="Feature with mixed results" name="Scenario that also passes" status="passed" time=".*">
+       <testcase classname="Feature with mixed results" name="Scenario that also passes" foldername="Scenario that also passes" status="passed" time=".*">
        </testcase>
-       <testcase classname="Feature with mixed results" name="Scenario that has an undefined step" status="failed" time=".*">
+       <testcase classname="Feature with mixed results" name="Scenario that has an undefined step" foldername="Scenario that has an undefined step" status="failed" time=".*">
        </testcase>
-       <testcase classname="Feature with mixed results" name="Scenario that is skipped" status="skipped" time=".*" tags="disabled">
+       <testcase classname="Feature with mixed results" name="Scenario that is skipped" foldername="Scenario that is skipped" status="skipped" time=".*" tags="disabled">
         <skipped>
         </skipped>
        </testcase>

features/cli/run_with_report.feature
@@ -18,3 +18,12 @@ Feature: Run with report
      Then I should see the link "Feature with failing scenario"
      When I click the link "Feature with failing scenario"
      Then I should see the link "Just a scenario that fails"
+
+  Scenario: User can generate a correct report with long feature name and scenario name
+    Given I run the command "cucu run data/features/feature_with_long_names.feature --results {CUCU_RESULTS_DIR}/run_with_report_results --generate-report --report {CUCU_RESULTS_DIR}/run_with_report_report" and save stdout to "STDOUT", stderr to "STDERR" and expect exit code "0"
+      And I start a webserver at directory "{CUCU_RESULTS_DIR}/run_with_report_report/" and save the port to the variable "PORT"
+      And I open a browser at the url "http://{HOST_ADDRESS}:{PORT}/index.html"
+     Then I should see the link "Feature with an absolutely unnecessarily, ridiculously, comically, I don't know what they're thinking, long name"
+     When I click the link "Feature with an absolutely unnecessarily, ridiculously, comically, I don't know what they're thinking, long name"
+     Then I should see the link "Scenario with an absolutely unnecessarily, ridiculously, comically, I don't know what they're thinking, long name"
+      And I click the link "Scenario with an absolutely unnecessarily, ridiculously, comically, I don't know what they're thinking, long name"

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "cucu"
-version = "0.194.0"
+version = "0.195.0"
 license = "MIT"
 description = "Easy BDD web testing"
 authors = ["Domino Data Lab <open-source@dominodatalab.com>"]

src/cucu/formatter/junit.py
@@ -11,6 +11,7 @@ from bs4.formatter import XMLFormatter
 from tenacity import RetryError
 
 from cucu.config import CONFIG
+from cucu.utils import ellipsize_filename
 
 
 class CucuJUnitFormatter(Formatter):
@@ -38,6 +39,7 @@ class CucuJUnitFormatter(Formatter):
         date_now = datetime.now()
         self.feature_results = {
             "name": escape(feature.name),
+            "foldername": escape(ellipsize_filename(feature.name)),
             "tests": 0,
             "errors": 0,
             "failures": 0,
@@ -115,6 +117,9 @@ class CucuJUnitFormatter(Formatter):
             "failure": None,
             "skipped": None,
         }
+        self.current_scenario_results["foldername"] = escape(
+            ellipsize_filename(scenario.name)
+        )
         if scenario.tags:
             self.current_scenario_results["tags"] = ", ".join(scenario.tags)
 
@@ -123,7 +128,7 @@ class CucuJUnitFormatter(Formatter):
             scenario_name
         ] = self.current_scenario_results
 
-        # we write out every new scenario into the JUnit results output which
+        # we write out every new scenario into the JUnit results output
         # which allows us to have a valid JUnit XML results file per feature
         # file currently running even if the process crashes or is killed so we
         # can still generate valid reports from every run.
@@ -164,6 +169,7 @@ class CucuJUnitFormatter(Formatter):
         given a feature results dictionary that looks like so:
             {
                 "name": "name of the feature",
+                "foldername": "",
                 "tests": 0,
                 "errors": 0,
                 "failures": 0,
@@ -171,6 +177,7 @@ class CucuJUnitFormatter(Formatter):
                 "timestamp": "",
                 "scenarios": {
                     "scenario name": {
+                        "foldername": "",
                         "tags": "DOM-3435, testrail(3366,45891)",
                         "status": "passed/failed/skipped",
                         "time": "0.0000":
@@ -189,6 +196,7 @@ class CucuJUnitFormatter(Formatter):
                 ordered = [
                     "classname",
                     "name",
+                    "foldername",
                     "tests",
                     "errors",
                     "failures",
@@ -206,6 +214,7 @@ class CucuJUnitFormatter(Formatter):
         soup = bs4.BeautifulSoup()
         testsuite = bs4.Tag(name="testsuite")
         testsuite["name"] = results["name"]
+        testsuite["foldername"] = results["foldername"]
         testsuite["timestamp"] = results["timestamp"]
 
         junit_dir = CONFIG["CUCU_JUNIT_DIR"]
@@ -255,6 +264,7 @@ class CucuJUnitFormatter(Formatter):
             testcase = bs4.Tag(name="testcase")
             testcase["classname"] = results["name"]
             testcase["name"] = scenario_name
+            testcase["foldername"] = scenario["foldername"]
             if "tags" in scenario:
                 testcase["tags"] = scenario["tags"]
             testcase["status"] = scenario["status"]

src/cucu/reporter/html.py
@@ -100,6 +100,7 @@ def generate(results, basepath, only_failures=False):
     CONFIG.snapshot()
     reported_features = []
     for feature in features:
+        feature["folder_name"] = ellipsize_filename(feature["name"])
         if show_status:
             print("F", end="", flush=True)
         scenarios = []
@@ -123,11 +124,9 @@ def generate(results, basepath, only_failures=False):
 
         if feature["status"] not in ["skipped", "untested"]:
             # copy each feature directories contents over to the report directory
-            src_feature_filepath = os.path.join(
-                results, ellipsize_filename(feature["name"])
-            )
+            src_feature_filepath = os.path.join(results, feature["folder_name"])
             dst_feature_filepath = os.path.join(
-                basepath, ellipsize_filename(feature["name"])
+                basepath, feature["folder_name"]
             )
             shutil.copytree(
                 src_feature_filepath, dst_feature_filepath, dirs_exist_ok=True
@@ -136,10 +135,11 @@ def generate(results, basepath, only_failures=False):
         for scenario in scenarios:
             CONFIG.restore()
 
+            scenario["folder_name"] = ellipsize_filename(scenario["name"])
             scenario_filepath = os.path.join(
                 basepath,
-                ellipsize_filename(feature["name"]),
-                ellipsize_filename(scenario["name"]),
+                feature["folder_name"],
+                scenario["folder_name"],
             )
 
             scenario_configpath = os.path.join(
@@ -399,7 +399,7 @@ def generate(results, basepath, only_failures=False):
     feature_template = templates.get_template("feature.html")
 
     for feature in reported_features:
-        feature_basepath = os.path.join(basepath, feature["name"])
+        feature_basepath = os.path.join(basepath, feature["folder_name"])
         os.makedirs(feature_basepath, exist_ok=True)
 
         scenarios = []
@@ -424,7 +424,9 @@ def generate(results, basepath, only_failures=False):
 
         for scenario in scenarios:
             steps = scenario["steps"]
-            scenario_basepath = os.path.join(feature_basepath, scenario["name"])
+            scenario_basepath = os.path.join(
+                feature_basepath, scenario["folder_name"]
+            )
             os.makedirs(scenario_basepath, exist_ok=True)
 
             scenario_output_filepath = os.path.join(

src/cucu/reporter/templates/feature.html
@@ -55,7 +55,7 @@
                 {% endif %}
                 </td>
                 <td>
-                    <a href="{{ urlencode(escape(feature['name'])) }}/{{ urlencode(escape(scenario['name'])) }}/index.html"><span style="display: inline; color: grey">{{ escape(scenario['name']) }}</span></a><br/>
+                    <a href="{{ urlencode(escape(feature['folder_name'])) }}/{{ urlencode(escape(scenario['folder_name'])) }}/index.html"><span style="display: inline; color: grey">{{ escape(scenario['name']) }}</span></a><br/>
                     <span style="display: inline; color: darkslateblue">{{ scenario['tags'] }}</span>
                 </td>
                 <td class="text-center">{{ scenario['total_steps'] }}</td>

src/cucu/reporter/templates/flat.html
@@ -32,7 +32,7 @@
                 <tr>
                     <td class="text-center">{{ scenario['started_at'] }}</td>
                     <td><a href="{{ urlencode(escape(feature['name'])) }}.html"><span>{{ escape(feature['name']) }}</span></a><br>{{ feature['tags'] }}</td>
-                    <td><a href="{{ urlencode(escape(feature['name'])) }}/{{ urlencode(escape(scenario['name'])) }}/index.html"><span>{{ escape(scenario['name']) }}</span></a><br>{{ scenario['tags'] }}</td>
+                    <td><a href="{{ urlencode(escape(feature['folder_name'])) }}/{{ urlencode(escape(scenario['folder_name'])) }}/index.html"><span>{{ escape(scenario['name']) }}</span></a><br>{{ scenario['tags'] }}</td>
                     <td class="text-center">{{ scenario['total_steps'] }}</td>
                     <td class="text-center"><span class="status-{{ scenario['status'] }}">{{ scenario['status'] }}</span></td>
                     <td class="text-center">{{ scenario['duration'] }}</td>

src/cucu/utils.py
@@ -180,6 +180,7 @@ def normalize_filename(raw_filename):
         .replace("{", "")
         .replace("}", "")
         .replace("#", "")
+        .replace("&", "")
     )
     return normalized_filename
 