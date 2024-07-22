3b3edd9ede58ad6e932d5ade566c68baa3675594
Cedric Young <Cedric.Young@dominodatalab.com>
2024-07-22 14:30:05 -0700

    fix test pickling issue

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,5 @@
 import time
 from multiprocessing import Process
-from unittest import mock
 
 from click.testing import CliRunner
 from cucu.cli import core
@@ -9,21 +8,19 @@ RUNTIME_TIMEOUT = 10
 
 
 def process_func_to_run_cucu(workers, runtime_timeout):
-    isdir_mock = mock.MagicMock(return_value=False)
-    with mock.patch("os.path.isdir", isdir_mock):
-        behave_mock = mock.MagicMock(side_effect=RuntimeError("something bad"))
-        with mock.patch("cucu.cli.core.behave", behave_mock):
-            runner = CliRunner()
-            runner.invoke(
-                core.run,
-                f"--runtime-timeout {runtime_timeout} --workers={workers} abc",
-            )
+    # not using mocks here since mocked `behave` doesn't pickle
+    failing_scenario = "data/features/feature_with_failing_scenario.feature"
+    runner = CliRunner()
+    runner.invoke(
+        core.run,
+        f"--runtime-timeout {runtime_timeout} --workers={workers} {failing_scenario}",
+    )
 
 
-def test_timeout_with_behave_exception_in_workers():
+def test_scenario_failure_shortcuts_runtime_timeout():
     start = time.time()
     p = Process(target=process_func_to_run_cucu, args=(2, RUNTIME_TIMEOUT))
     p.start()
     p.join()
     elapsed_time = time.time() - start
-    assert elapsed_time < 14
+    assert elapsed_time < 10
