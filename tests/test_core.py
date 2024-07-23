435acad82bee311c0147ceccf8e6dadca5eda954
Xin Dong <xin.dong@dominodatalab.com>
2024-07-23 13:25:59 -0700

    Update a unit test

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,6 @@
 import time
 from multiprocessing import Process
+from unittest import mock
 
 from click.testing import CliRunner
 from cucu.cli import core
@@ -8,13 +9,16 @@ RUNTIME_TIMEOUT = 10
 
 
 def process_func_to_run_cucu(workers, runtime_timeout):
-    # not using mocks here since mocked `behave` doesn't pickle
-    failing_scenario = "data/features/feature_with_failing_scenario.feature"
-    runner = CliRunner()
-    runner.invoke(
-        core.run,
-        f"--runtime-timeout {runtime_timeout} --workers={workers} {failing_scenario}",
-    )
+    isdir_mock = mock.MagicMock(return_value=False)
+    with mock.patch("os.path.isdir", isdir_mock):
+        config_mock = mock.MagicMock()
+        config_mock.load_cucurc_files = mock.MagicMock(side_effect=RuntimeError("something bad"))
+        with mock.patch("cucu.cli.run.CONFIG", config_mock):
+            runner = CliRunner()
+            runner.invoke(
+                core.run,
+                f"--runtime-timeout {runtime_timeout} --workers={workers} abc",
+            )
 
 
 def test_subprocess_failure_shortcuts_runtime_timeout():
@@ -23,4 +27,4 @@ def test_subprocess_failure_shortcuts_runtime_timeout():
     p.start()
     p.join()
     elapsed_time = time.time() - start
-    assert elapsed_time < 10
+    assert elapsed_time < 5
