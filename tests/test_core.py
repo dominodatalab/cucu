cb6f1be185cb64dce48ac28cd48d22a5616619d8
Cedric Young <Cedric.Young@dominodatalab.com>
2024-07-23 11:04:40 -0700

    restore not-mocked test

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,5 @@
 import time
 from multiprocessing import Process
-from unittest import mock
 
 from click.testing import CliRunner
 from cucu.cli import core
@@ -9,23 +8,19 @@ RUNTIME_TIMEOUT = 10
 
 
 def process_func_to_run_cucu(workers, runtime_timeout):
-    isdir_mock = mock.MagicMock(return_value=False)
-    with mock.patch("os.path.isdir", isdir_mock):
-        behave_mock = mock.MagicMock(side_effect=RuntimeError("something bad"))
-        with mock.patch.object(
-            "cucu.cli.core.CONFIG", "load_cucurc_files", behave_mock
-        ):
-            runner = CliRunner()
-            runner.invoke(
-                core.run,
-                f"--runtime-timeout {runtime_timeout} --workers={workers} abc",
-            )
+    # not using mocks here since mocked `behave` doesn't pickle
+    failing_scenario = "data/features/feature_with_failing_scenario.feature"
+    runner = CliRunner()
+    runner.invoke(
+        core.run,
+        f"--runtime-timeout {runtime_timeout} --workers={workers} {failing_scenario}",
+    )
 
 
-def test_behave_failure_shortcuts_runtime_timeout():
+def test_scenario_failure_shortcuts_runtime_timeout():
     start = time.time()
     p = Process(target=process_func_to_run_cucu, args=(2, RUNTIME_TIMEOUT))
     p.start()
     p.join()
     elapsed_time = time.time() - start
-    assert elapsed_time < 5
+    assert elapsed_time < 10
