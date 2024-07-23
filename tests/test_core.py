a33f54583963534c451e7ba5ff9e433e00ac8fe1
Xin Dong <xin.dong@dominodatalab.com>
2024-07-23 14:19:47 -0700

    Lint fix

--- tests/test_core.py
+++ tests/test_core.py
@@ -12,7 +12,9 @@ def process_func_to_run_cucu(workers, runtime_timeout):
     isdir_mock = mock.MagicMock(return_value=False)
     with mock.patch("os.path.isdir", isdir_mock):
         config_mock = mock.MagicMock()
-        config_mock.load_cucurc_files = mock.MagicMock(side_effect=RuntimeError("something bad"))
+        config_mock.load_cucurc_files = mock.MagicMock(
+            side_effect=RuntimeError("something bad")
+        )
         with mock.patch("cucu.cli.run.CONFIG", config_mock):
             runner = CliRunner()
             runner.invoke(
