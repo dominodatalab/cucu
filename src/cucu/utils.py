ec5ae730fb2248691d2fbeafb97fe2efda1023d0
ddl-kavya <kavya.yakkati@dominodatalab.com>
2025-06-05 15:21:42 +0530

    ddl-kavya-log tab and window information in the debug report

--- src/cucu/utils.py
+++ src/cucu/utils.py
@@ -277,5 +277,9 @@ def get_tab_information(ctx):
     window_handles = driver.window_handles
     current_window = driver.current_window_handle
     window_handle_index = window_handles.index(current_window)
-    logger.debug(f"Total open tabs/windows: {len(window_handles)}")
-    logger.debug(f"you are on tab: {window_handle_index + 1} with title: {driver.title} and url: {driver.current_url}")
+    logger.debug("total open tabs/windows: %d", len(window_handles))
+    logger.debug("You are on tab: %d with title: %s and url: %s",
+                 window_handle_index + 1,
+                 driver.title,
+                 driver.current_url
+                 )
