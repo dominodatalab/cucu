9ad19fb4415f2206b9d777399d01adb01f065f44
Martin Currao <curra.currao@dominodatalab.com>
2026-01-09 16:53:52 -0300

    chore(tests/table): Remove unit tests for tables
    Tests were moved to Cucu steps in the row count scenario

--- tests/test_table_steps.py
+++ tests/test_table_steps.py
@@ -1,13 +1,8 @@
-import operator
 from unittest import TestCase, mock
-
-import pytest
 
 from cucu.steps.table_steps import (
     check_table_contains_matching_rows_in_table,
     do_not_find_table,
-    find_nth_table_and_validate_row_count,
-    find_table_matching_rows_and_validate_row_count,
 )
 
 
@@ -58,106 +53,3 @@ class TestDoNotFindTable(TestCase):
                     dummy, check_table_contains_matching_rows_in_table
                 )
                 self.assertIsNone(result)
-
-
-@pytest.mark.parametrize(
-    "thing,check_func,expected_rows,actual_rows,should_raise",
-    [
-        ("more than", operator.gt, 2, 1, True),
-        ("more than", operator.gt, 2, 2, True),
-        ("more than", operator.gt, 2, 3, False),
-        ("equals", operator.eq, 2, 1, True),
-        ("equals", operator.eq, 2, 2, False),
-        ("at least", operator.ge, 2, 1, True),
-        ("at least", operator.ge, 2, 2, False),
-        ("at least", operator.ge, 2, 3, False),
-        ("at most", operator.le, 2, 1, False),
-        ("at most", operator.le, 2, 2, False),
-        ("at most", operator.le, 2, 3, True),
-        ("less than", operator.lt, 2, 1, False),
-        ("less than", operator.lt, 2, 2, True),
-        ("less than", operator.lt, 2, 3, True),
-        ("not equal to", operator.ne, 2, 2, True),
-        ("not equal to", operator.ne, 2, 3, False),
-    ],
-)
-def test_find_table_matching_rows_and_validate_row_count(
-    thing, check_func, expected_rows, actual_rows, should_raise
-):
-    with mock.patch(
-        "cucu.steps.table_steps.find_table", return_value=[[1]] * actual_rows
-    ):
-        dummy_ctx = mock.MagicMock()
-
-        if should_raise:
-            with pytest.raises(RuntimeError) as context:
-                find_table_matching_rows_and_validate_row_count(
-                    dummy_ctx, thing, check_func, expected_rows
-                )
-            assert f"Expected {thing} {expected_rows} rows" in str(
-                context.value
-            )
-            assert f"but found {actual_rows} instead" in str(context.value)
-        else:
-            # Should not raise any exception
-            result = find_table_matching_rows_and_validate_row_count(
-                dummy_ctx, thing, check_func, expected_rows
-            )
-            assert result is None
-
-
-@pytest.mark.parametrize(
-    "thing,check_func,expected_rows,actual_rows,should_raise",
-    [
-        ("more than", operator.gt, 2, 1, True),
-        ("more than", operator.gt, 2, 2, True),
-        ("more than", operator.gt, 2, 3, False),
-        ("equals", operator.eq, 2, 1, True),
-        ("equals", operator.eq, 2, 2, False),
-        ("at least", operator.ge, 2, 1, True),
-        ("at least", operator.ge, 2, 2, False),
-        ("at least", operator.ge, 2, 3, False),
-        ("at most", operator.le, 2, 1, False),
-        ("at most", operator.le, 2, 2, False),
-        ("at most", operator.le, 2, 3, True),
-        ("less than", operator.lt, 2, 1, False),
-        ("less than", operator.lt, 2, 2, True),
-        ("less than", operator.lt, 2, 3, True),
-        ("not equal to", operator.ne, 2, 2, True),
-        ("not equal to", operator.ne, 2, 3, False),
-    ],
-)
-def test_find_nth_table_and_validate_row_count(
-    thing, check_func, expected_rows, actual_rows, should_raise
-):
-    mock_table_element = mock.MagicMock()
-
-    with (
-        mock.patch(
-            "cucu.steps.table_steps.find_table_element",
-            return_value=mock_table_element,
-        ),
-        mock.patch(
-            "cucu.steps.table_steps.count_rows_in_table_element",
-            return_value=actual_rows,
-        ),
-    ):
-        dummy_ctx = mock.MagicMock()
-        table_index = 0
-
-        if should_raise:
-            with pytest.raises(RuntimeError) as context:
-                find_nth_table_and_validate_row_count(
-                    dummy_ctx, table_index, thing, check_func, expected_rows
-                )
-            assert (
-                f"Expected {thing} {expected_rows} rows in table {table_index + 1}"
-                in str(context.value)
-            )
-            assert f"but found {actual_rows} instead" in str(context.value)
-        else:
-            # Should not raise any exception
-            result = find_nth_table_and_validate_row_count(
-                dummy_ctx, table_index, thing, check_func, expected_rows
-            )
-            assert result is None
