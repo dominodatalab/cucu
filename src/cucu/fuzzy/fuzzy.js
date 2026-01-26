240813680896b1c5c016bdf03539f8b59f69d0d5
Cedric Young <4129217+ccedricyoung@users.noreply.github.com>
2026-01-26 13:14:45 -0800

    nit rename

--- src/cucu/fuzzy/fuzzy.js
+++ src/cucu/fuzzy/fuzzy.js
@@ -253,7 +253,7 @@
         for (var i = 0; i < elements.length; i++) {
             if (seen_elements.indexOf(elements[i].element) === -1) {
                 seen_elements.push(elements[i].element);
-                elements[i].org_index = i;
+                elements[i].pass = i;
                 deduped_elements.push(elements[i]);
             }
         }
@@ -263,7 +263,7 @@
         for (var i = 0; i < elements.length; i++) {
             const rect = elements[i].element.getBoundingClientRect();
             const content = (elements[i].element.textContent || elements[i].element.innerText || jqCucu(elements[i].element).text() || '').replace(/\n/g, '').trim();
-            debugMsg += `\n  [${i}]: text '${content}' at (${Math.round(rect.x)}, ${Math.round(rect.y)}) pass [${elements[i].org_index}] for ${elements[i].label_name} using ${elements[i].label}`;
+            debugMsg += `\n  [${i}]: text '${content}' at (${Math.round(rect.x)}, ${Math.round(rect.y)}) pass [${elements[i].pass}] for ${elements[i].label_name} using ${elements[i].label}`;
         }
         console.debug(debugMsg);
 
